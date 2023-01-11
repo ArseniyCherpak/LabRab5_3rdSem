ebffdadcb6f401ff541626d826035e34480993fc
ArseniyCherpak <122453713+ArseniyCherpak@users.noreply.github.com>
2023-01-11 18:05:09 +0500

    Рабочая версия. Сделано на Java 19

---
+++ LabProg_simple_web/src/test/java/com/example/labprog_simple_web/LabProgSimpleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labprog_simple_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabProgSimpleWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
